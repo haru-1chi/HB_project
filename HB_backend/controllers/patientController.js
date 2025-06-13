d500642a71087debc6021a62f2243b1d292ede0f
Haru Ichi <jarunee.recc2@gmail.com>
2025-06-13 15:37:29 +0700

    update avg_drug and all

--- HB_backend/controllers/patientController.js
+++ HB_backend/controllers/patientController.js
@@ -106,122 +106,130 @@ async function stateOPDS(req, res) {
         o.PLA_PLACECODE,
         pl.fullplace AS OPD_name,
          COUNT(DISTINCT o.OPD_NO) AS all_user,
-              SUM(
-  CASE 
-    WHEN TO_CHAR(o.OPD_TIME, 'HH24:MI:SS') != '00:00:00' AND o.FINISH_OPD_DATETIME IS NULL 
-    THEN 1 
-    ELSE 0 
-  END
-) AS WAIT_PTS,
-    SUM(CASE WHEN o.OPD_TIME = TO_DATE('00:00:00', 'HH24:MI:SS') THEN 1 ELSE 0 END) AS NOSHOW_PTS,
+	SUM( CASE WHEN TO_CHAR( o.OPD_TIME, 'HH24:MI:SS' ) != '00:00:00' AND o.FINISH_OPD_DATETIME IS NULL THEN 1 ELSE 0 END ) AS WAIT_PTS,
+	SUM( CASE WHEN TO_CHAR( o.OPD_TIME, 'HH24:MI:SS' ) = '00:00:00' THEN 1 ELSE 0 END ) AS NOSHOW_PTS,
         SUM(CASE WHEN o.FINISH_OPD_DATETIME IS NULL THEN 1 ELSE 0 END) AS pending,
         SUM(CASE WHEN o.FINISH_OPD_DATETIME IS NOT NULL THEN 1 ELSE 0 END) AS completed,
         ROUND(AVG(
             CASE 
                 WHEN o.RX_OPD_DATETIME IS NOT NULL 
-                THEN ABS((o.RX_OPD_DATETIME - o.SCREENING_OPD_DATETIME) * 24 * 60) 
+                THEN ABS((o.RX_OPD_DATETIME - o.REACH_OPD_DATETIME) * 24 * 60) 
                 ELSE NULL 
             END
         ), 2) AS avg_wait_screen,
         ROUND(AVG(
             CASE 
                 WHEN od.ALREADY_RECEIVE_DRUG_DATE IS NOT NULL 
-                THEN ABS((od.ALREADY_RECEIVE_DRUG_DATE - o.AFTER_DOC_DATETIME) * 24 * 60) 
+                THEN ABS((od.ALREADY_RECEIVE_DRUG_DATE - od.DATE_AND_TIME) * 24 * 60) 
                 ELSE NULL 
             END
         ), 2) AS avg_wait_drug,
-        ROUND(AVG(
-            CASE 
-                WHEN o.FINISH_OPD_DATETIME IS NOT NULL 
-                THEN ABS((o.FINISH_OPD_DATETIME - o.REACH_OPD_DATETIME) * 24 * 60) 
-                ELSE NULL 
-            END
-        ), 2) AS avg_wait_all
-      FROM OPDS o
-      JOIN PLACES pl ON pl.PLACECODE = o.PLA_PLACECODE
-      LEFT JOIN (
-  SELECT *
-  FROM (
-    SELECT od.*, ROW_NUMBER() OVER (PARTITION BY od.opd_no ORDER BY od.ALREADY_RECEIVE_DRUG_DATE DESC) AS rn
-    FROM OPD_FINANCE_HEADERS od
-  )
-  WHERE rn = 1
-) od ON od.opd_no = o.opd_no
-      WHERE o.opd_date = TRUNC(SYSDATE) ${opdFilter}
+ROUND(
+  AVG(
+    CASE
+      WHEN od.ALREADY_RECEIVE_DRUG_DATE IS NOT NULL THEN
+        ABS((od.ALREADY_RECEIVE_DRUG_DATE - o.REACH_OPD_DATETIME) * 24 * 60)
+      WHEN o.FINISH_OPD_DATETIME IS NOT NULL THEN
+        ABS((o.FINISH_OPD_DATETIME - o.REACH_OPD_DATETIME) * 24 * 60)
+      ELSE NULL
+    END
+  ), 2
+) AS avg_wait_all
+     FROM
+				OPDS o
+				JOIN PLACES pl ON pl.PLACECODE = o.PLA_PLACECODE
+				LEFT JOIN (
+				SELECT
+					* 
+				FROM
+					(
+					SELECT
+						od.*,
+						ROW_NUMBER ( ) OVER ( PARTITION BY opd_no ORDER BY ALREADY_RECEIVE_DRUG_DATE DESC ) AS rn 
+					FROM
+						OPD_FINANCE_HEADERS od 
+					WHERE
+						od.ALREADY_RECEIVE_DRUG_DATE IS NOT NULL 
+					) 
+				WHERE
+					rn = 1 
+				) od ON od.opd_no = o.opd_no 
+			WHERE
+				o.opd_date = TRUNC( SYSDATE ) ${opdFilter}
       GROUP BY o.PLA_PLACECODE, pl.fullplace
       ORDER BY ${sortField} ${sortOrder}`;
 
-//        const query = `
-//     WITH base_data AS (
-//   SELECT 
-//     DISTINCT o.OPD_NO, 
-//     o.PLA_PLACECODE,
-//     pl.fullplace AS OPD_name,
-//     o.OPD_TIME,
-//     o.FINISH_OPD_DATETIME,
-//     o.RX_OPD_DATETIME,
-//     o.SCREENING_OPD_DATETIME,
-//     o.REACH_OPD_DATETIME,
-//     o.AFTER_DOC_DATETIME,
-//     od.ALREADY_RECEIVE_DRUG_DATE
-//   FROM OPDS o
-//   JOIN PLACES pl ON pl.PLACECODE = o.PLA_PLACECODE
-//   LEFT JOIN OPD_FINANCE_HEADERS od ON od.opd_no = o.opd_no
-//   WHERE o.opd_date = TRUNC(SYSDATE) ${opdFilter}
-// )
+    //        const query = `
+    //     WITH base_data AS (
+    //   SELECT 
+    //     DISTINCT o.OPD_NO, 
+    //     o.PLA_PLACECODE,
+    //     pl.fullplace AS OPD_name,
+    //     o.OPD_TIME,
+    //     o.FINISH_OPD_DATETIME,
+    //     o.RX_OPD_DATETIME,
+    //     o.SCREENING_OPD_DATETIME,
+    //     o.REACH_OPD_DATETIME,
+    //     o.AFTER_DOC_DATETIME,
+    //     od.ALREADY_RECEIVE_DRUG_DATE
+    //   FROM OPDS o
+    //   JOIN PLACES pl ON pl.PLACECODE = o.PLA_PLACECODE
+    //   LEFT JOIN OPD_FINANCE_HEADERS od ON od.opd_no = o.opd_no
+    //   WHERE o.opd_date = TRUNC(SYSDATE) ${opdFilter}
+    // )
 
-// SELECT  
-//   b.PLA_PLACECODE,
-//   b.OPD_name,
-//   COUNT(b.OPD_NO) AS all_user,
+    // SELECT  
+    //   b.PLA_PLACECODE,
+    //   b.OPD_name,
+    //   COUNT(b.OPD_NO) AS all_user,
 
-//   SUM(
-//     CASE 
-//       WHEN TO_CHAR(b.OPD_TIME, 'HH24:MI:SS') != '00:00:00' AND b.FINISH_OPD_DATETIME IS NULL 
-//       THEN 1 
-//       ELSE 0 
-//     END
-//   ) AS WAIT_PTS,
+    //   SUM(
+    //     CASE 
+    //       WHEN TO_CHAR(b.OPD_TIME, 'HH24:MI:SS') != '00:00:00' AND b.FINISH_OPD_DATETIME IS NULL 
+    //       THEN 1 
+    //       ELSE 0 
+    //     END
+    //   ) AS WAIT_PTS,
 
-//   SUM(
-//     CASE 
-//       WHEN TO_CHAR(b.OPD_TIME, 'HH24:MI:SS') = '00:00:00'
-//       THEN 1 
-//       ELSE 0 
-//     END
-//   ) AS NOSHOW_PTS,
+    //   SUM(
+    //     CASE 
+    //       WHEN TO_CHAR(b.OPD_TIME, 'HH24:MI:SS') = '00:00:00'
+    //       THEN 1 
+    //       ELSE 0 
+    //     END
+    //   ) AS NOSHOW_PTS,
 
-//   SUM(CASE WHEN b.FINISH_OPD_DATETIME IS NULL THEN 1 ELSE 0 END) AS pending,
-//   SUM(CASE WHEN b.FINISH_OPD_DATETIME IS NOT NULL THEN 1 ELSE 0 END) AS completed,
+    //   SUM(CASE WHEN b.FINISH_OPD_DATETIME IS NULL THEN 1 ELSE 0 END) AS pending,
+    //   SUM(CASE WHEN b.FINISH_OPD_DATETIME IS NOT NULL THEN 1 ELSE 0 END) AS completed,
 
-//   ROUND(AVG(
-//       CASE 
-//           WHEN b.RX_OPD_DATETIME IS NOT NULL 
-//           THEN ABS((b.RX_OPD_DATETIME - b.SCREENING_OPD_DATETIME) * 24 * 60) 
-//           ELSE NULL 
-//       END
-//   ), 2) AS avg_wait_screen,
+    //   ROUND(AVG(
+    //       CASE 
+    //           WHEN b.RX_OPD_DATETIME IS NOT NULL 
+    //           THEN ABS((b.RX_OPD_DATETIME - b.SCREENING_OPD_DATETIME) * 24 * 60) 
+    //           ELSE NULL 
+    //       END
+    //   ), 2) AS avg_wait_screen,
 
-//   ROUND(AVG(
-//       CASE 
-//           WHEN b.ALREADY_RECEIVE_DRUG_DATE IS NOT NULL 
-//           THEN ABS((b.ALREADY_RECEIVE_DRUG_DATE - b.AFTER_DOC_DATETIME) * 24 * 60) 
-//           ELSE NULL 
-//       END
-//   ), 2) AS avg_wait_drug,
+    //   ROUND(AVG(
+    //       CASE 
+    //           WHEN b.ALREADY_RECEIVE_DRUG_DATE IS NOT NULL 
+    //           THEN ABS((b.ALREADY_RECEIVE_DRUG_DATE - b.AFTER_DOC_DATETIME) * 24 * 60) 
+    //           ELSE NULL 
+    //       END
+    //   ), 2) AS avg_wait_drug,
 
-//   ROUND(AVG(
-//       CASE 
-//           WHEN b.FINISH_OPD_DATETIME IS NOT NULL 
-//           THEN ABS((b.FINISH_OPD_DATETIME - b.REACH_OPD_DATETIME) * 24 * 60) 
-//           ELSE NULL 
-//       END
-//   ), 2) AS avg_wait_all
+    //   ROUND(AVG(
+    //       CASE 
+    //           WHEN b.FINISH_OPD_DATETIME IS NOT NULL 
+    //           THEN ABS((b.FINISH_OPD_DATETIME - b.REACH_OPD_DATETIME) * 24 * 60) 
+    //           ELSE NULL 
+    //       END
+    //   ), 2) AS avg_wait_all
 
-// FROM base_data b
-// GROUP BY b.PLA_PLACECODE, b.OPD_name
-//   ORDER BY ${sortField} ${sortOrder}
-// `;
+    // FROM base_data b
+    // GROUP BY b.PLA_PLACECODE, b.OPD_name
+    //   ORDER BY ${sortField} ${sortOrder}
+    // `;
 
     // Use executeMany() for correct binding
     const result = await connection.execute(query, queryParams, { autoCommit: true });
